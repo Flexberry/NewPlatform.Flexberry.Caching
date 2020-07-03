e84c996247dff6c5e68a553b8039b5bdd6b857a2
Tsar Nikolay <nsmirnov@ics.perm.ru>
2020-07-03 09:42:29 +0500

    Add Trim method to remove entries. Test memory leak. Test parallel and multithread.

---
+++ NewPlatform.Flexberry.Caching.Tests/MemoryCacheService/MemoryCacheServiceMemoryLeakageTests.cs
@@ -0,0 +1,157 @@
+﻿namespace NewPlatform.Flexberry.Caching.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+
+    using Xunit;
+
+    public class MemoryCacheServiceMemoryLeakageTests
+    {
+        private const int ExpiratoinTime = 3; // In seconds.
+
+        private const string CacheName = "expiration";
+
+        /// <summary>
+        /// Test memory leakage on expired values.
+        /// </summary>
+        [Fact(Skip = "Manual testing")]
+        public void MemoryLeakageOnExpirationTest()
+        {
+            // Arrange.
+            const int iterations = 100;
+            const int length = 10000;
+            int[] range = Enumerable.Range(0, length).ToArray();
+
+            CleanupMemory();
+
+            // Act.
+            var cacheService = new MemoryCacheService(CacheName, ExpiratoinTime);
+            IList<WeakReference> wrList = ProcessCacheService(cacheService, iterations, range);
+
+            // Sleep for one more second after expiration time.
+            Thread.Sleep((ExpiratoinTime + 1) * 1000);
+
+            // Force remove expired entries.
+            long removed = cacheService.Trim();
+            Assert.True(removed <= iterations);
+
+            CleanupMemory();
+
+            // Assert.
+            ValidateWeakReference(wrList);
+        }
+
+        /// <summary>
+        /// Test memory leakage on expired values with checking existance.
+        /// </summary>
+        [Fact(Skip = "Manual testing")]
+        public void MemoryLeakageOnExpirationWithCheckTest()
+        {
+            // Arrange.
+            const int iterations = 100;
+            const int length = 10000;
+            int[] range = Enumerable.Range(0, length).ToArray();
+
+            CleanupMemory();
+
+            // Act.
+            var cacheService = new MemoryCacheService(CacheName, ExpiratoinTime);
+            IList<WeakReference> wrList = ProcessCacheService(cacheService, iterations, range);
+
+            // Sleep for one more second after expiration time.
+            Thread.Sleep((ExpiratoinTime + 1) * 1000);
+
+            CleanupMemory();
+
+            // Check global expiration.
+            ValidateExpiration(cacheService, wrList);
+
+            CleanupMemory();
+
+            // Assert.
+            ValidateWeakReference(wrList);
+        }
+
+        private static void CleanupMemory()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        private static IList<WeakReference> ProcessCacheService(ICacheService cacheService, int iterations, int[] range, bool parallel = false)
+        {
+            // Act.
+            var iterRange = Enumerable.Range(0, iterations);
+            return parallel
+                ? iterRange.AsParallel().Select(i => IterateWeakReference(cacheService, range)).ToList()
+                : iterRange.Select(i => IterateWeakReference(cacheService, range)).ToList();
+        }
+
+        private static WeakReference IterateWeakReference(ICacheService cacheService, IEnumerable<int> range)
+        {
+            var obj = Iterate(cacheService, range);
+            return new WeakReference(obj);
+        }
+
+        private static SomeClass Iterate(ICacheService cacheService, IEnumerable<int> range)
+        {
+            // Arrange.
+            string key = Guid.NewGuid().ToString("D");
+            const int expectedIntValue = 101;
+            const string expectedStringValue = "String value new";
+
+            var newItem = new SomeClass
+            {
+                Key = key,
+                IntValue = expectedIntValue,
+                StringValue = expectedStringValue,
+                Tags = range.Select(i => $"Tag #{i} ({Guid.NewGuid()}{Guid.NewGuid()}{Guid.NewGuid()})").ToList(),
+            };
+
+            // Act.
+            if (!cacheService.SetToCache(key, newItem))
+            {
+                throw new Exception("Error while adding item to cache.");
+            }
+
+            // Assert.
+            Assert.True(cacheService.Exists(key));
+
+            return newItem;
+        }
+
+        private static void ValidateExpiration(ICacheService cacheService, IList<WeakReference> wrList)
+        {
+            foreach (var wr in wrList)
+            {
+                if (wr.IsAlive)
+                {
+                    if (wr.Target is SomeClass obj)
+                    {
+                        Assert.False(cacheService.Exists(obj.Key));
+                    }
+                    else
+                    {
+                        throw new Exception("WR указавает на объект некорректного типа.");
+                    }
+                }
+            }
+        }
+
+        private static void ValidateWeakReference(IList<WeakReference> wrList)
+        {
+            Assert.All(
+                wrList,
+                wr =>
+                {
+                    if (wr.IsAlive)
+                    {
+                        throw new Exception("GC не смог освободить объект.");
+                    }
+                });
+        }
+    }
+}
---
+++ NewPlatform.Flexberry.Caching.Tests/MemoryCacheService/MemoryCacheServiceMultiThreadTests.cs
@@ -0,0 +1,128 @@
+﻿namespace NewPlatform.Flexberry.Caching.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+
+    using NewPlatform.Flexberry.ORM.Tests;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    /// <summary>
+    /// Многопоточные тесты <see cref="MemoryCacheService" />.
+    /// </summary>
+    public class MemoryCacheServiceMultiThreadTests
+    {
+        private const int ExpiratoinTime = 3; // In seconds.
+
+        private const string CacheName = "multithread";
+
+        private readonly ITestOutputHelper Output;
+
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        public MemoryCacheServiceMultiThreadTests(ITestOutputHelper output)
+        {
+            Output = output;
+        }
+
+        /// <summary>
+        /// Многопоточный тест для <see cref="ParallelEnumerable.AsParallel" />.
+        /// </summary>
+        [Fact]
+        public void GetSetAsParallelTest()
+        {
+            // Arrange.
+            const int length = 100;
+            IEnumerable<int> range = Enumerable.Range(0, length);
+            var cacheService = new MemoryCacheService(CacheName, ExpiratoinTime);
+
+            // Act.
+            range.AsParallel().ForAll(i => Action(cacheService, i));
+        }
+
+        /// <summary>
+        /// Многопоточный тест с использованием <see cref="MultiThreadingTestTool"/>.
+        /// </summary>
+        [Fact]
+        public void GetSetMultiThreadToolTest()
+        {
+            // Arrange.
+            const int length = 100;
+            var cacheService = new MemoryCacheService(CacheName, ExpiratoinTime);
+            var multiThreadingTestTool = new MultiThreadingTestTool(MultiThreadMethod);
+            multiThreadingTestTool.StartThreads(length, cacheService);
+
+            // Assert.
+            var exception = multiThreadingTestTool.GetExceptions();
+            if (exception != null)
+            {
+                foreach (var item in exception.InnerExceptions)
+                {
+                    Output.WriteLine($"Thread {item.Key}: {item.Value}");
+                }
+
+                // Пусть так.
+                Assert.Empty(exception.InnerExceptions);
+            }
+        }
+
+        private static void MultiThreadMethod(object sender)
+        {
+            var parametersDictionary = sender as Dictionary<string, object>;
+            ICacheService cacheService = parametersDictionary[MultiThreadingTestTool.ParamNameSender] as ICacheService;
+            Dictionary<string, Exception> exceptions = parametersDictionary[MultiThreadingTestTool.ParamNameExceptions] as Dictionary<string, Exception>;
+
+            // Act.
+            try
+            {
+                Action(cacheService);
+            }
+            catch (Exception exception)
+            {
+                exceptions.Add(Thread.CurrentThread.Name, exception);
+                parametersDictionary[MultiThreadingTestTool.ParamNameWorking] = false;
+                return;
+            }
+        }
+
+        private static void Action(ICacheService cacheService, int i = 0)
+        {
+            // Arrange.
+            var obj = CreateObject(i);
+
+            // Act.
+            if (!cacheService.SetToCache(obj.Key, obj))
+            {
+                throw new Exception("Error while adding item to cache.");
+            }
+
+            var objFromCache = cacheService.GetFromCache(obj.Key);
+
+            // Assert.
+            Assert.NotNull(objFromCache);
+            Assert.Equal(obj, objFromCache);
+        }
+
+        private static SomeClass CreateObject(int i = 0)
+        {
+            // Arrange.
+            string key = Guid.NewGuid().ToString("D");
+            const int expectedIntValue = 101;
+            const string expectedStringValue = "String value new";
+
+            var newItem = new SomeClass
+            {
+                Key = key,
+                IntValue = expectedIntValue,
+                StringValue = expectedStringValue,
+                Tags = new List<string> { $"Tag #{i} ({Guid.NewGuid()}{Guid.NewGuid()}{Guid.NewGuid()})" },
+            };
+
+            return newItem;
+        }
+    }
+}
